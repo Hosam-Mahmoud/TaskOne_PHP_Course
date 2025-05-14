7e7f1622f57ffece9f4e6f7736e9c8120992d23e
Hosam-Mahmoud <hosamsalem778@gmail.com>
2025-05-14 19:38:29 +0300

    Add files via upload

---
+++ TaskOne.php
@@ -0,0 +1,38 @@
+<?php
+
+//---------------------------------//
+//------------Task One-------------//
+
+
+//---------------------------------//
+//------------variables------------//
+$sum = 29;
+
+$var1 = 10;
+
+$var2 = 20;
+
+
+//----------------------------------------------//
+//-----------mathematical operations------------//
+
+$result1= $var1 + $var2;
+
+$result2= $var1 - $var2;
+
+$result3= $var1 * $var2;
+
+$result4= $var1 / $var2;
+
+$result5= $var1 % $var2;
+
+$sum += $var1;
+
+echo $result1 . "<br>";
+echo $result2 . "<br>";
+echo $result3 . "<br>";
+echo $result4 . "<br>";
+echo $result5 . "<br>";
+echo $sum ;
+
+?>
